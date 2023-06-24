8549c5d5e591d8fb573b8eab86e329c6e32e624c
Mani <manikantavaddi4070@gmail.com>
2023-06-24 10:17:42 +0000

    schedulers-with-hooks

---
+++ src/components/Clock/index.js
@@ -0,0 +1,32 @@
+import {useState, useEffect} from 'react'
+import {ClockContainer, Heading, ClockImage, Time} from './styledComponents'
+
+const Clock = () => {
+  const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+    const timeId = setInterval(() => {
+      setDate(new Date())
+    }, 1000)
+    return () => {
+      clearInterval(timeId)
+    }
+  }, [])
+
+  useEffect(() => {
+    document.title = `Time:${date.toLocaleTimeString()}`
+  })
+
+  return (
+    <ClockContainer>
+      <Heading>Clock</Heading>
+      <ClockImage
+        src="https://assets.ccbp.in/frontend/hooks/clock-img.png"
+        alt="clock"
+      />
+      <Time> {date.toLocaleTimeString()} </Time>
+    </ClockContainer>
+  )
+}
+
+export default Clock
